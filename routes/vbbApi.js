04932c9d5ff1db79dceab256730c71595906c08f
Alexander Hans <mail@alexanderhans.me>
2018-03-07 20:14:04 +0100

    added getDepartures route

--- routes/vbbApi.js
+++ routes/vbbApi.js
@@ -30,6 +30,40 @@ router.get('/searchLocations/:searchInput', function(req, res) {
 	}
 });
 
+router.get('/getDepartures/:extId', function(req, res) {
+	try {
+		console.info('Retrieving VBB departures');
+		let getDeparturesOptions = {
+			method: 'GET',
+			url: buildUrl('departureBoard', {
+				extId: req.params.extId,
+				maxJourneys: 6,
+			}),
+			json: true,
+		};
+
+		request(getDeparturesOptions)
+		.then(function (parsedBody) {
+			res.send(parsedBody.Departure.map((departure) => {
+				return {
+					name: departure.name,
+					time: departure.time,
+					rtime: departure.rtime,
+					direction: departure.direction,
+					trainCategory: departure.trainCategory,
+				}
+			}))
+		})
+		.catch(function (e) {
+			console.error(e);
+			res.send('Error while retrieving VBB departures.');
+		});
+	} catch (ex) {
+		console.error(ex);
+		res.send('Error while retrieving VBB departures.');
+	}
+});
+
 module.exports = router;
 
 const buildUrl = (path, parameterObj = {}) => {
