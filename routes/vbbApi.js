7989b5ae52dd1e438460f9bfde46bac4cc1e5946
Alexander Hans <mail@alexanderhans.me>
2018-03-22 23:30:31 +0100

    added option to filter for direction

--- routes/vbbApi.js
+++ routes/vbbApi.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request-promise');
+const _ = require('lodash');
 
 const vbbApiBaseUrl = 'http://demo.hafas.de/openapi/vbb-proxy/';
 
@@ -39,13 +40,14 @@ router.get('/getDepartures/:extId/:products?', function(req, res) {
 				extId: req.params.extId,
 				maxJourneys: 6,
 				products: req.params.products || null,
+				direction: req.query.direction || null,
 			}),
 			json: true,
 		};
 
 		request(getDeparturesOptions)
 		.then(function (parsedBody) {
-			if (!parsedBody.Departure) {
+			if (!parsedBody.Departure || parsedBody.Departure === []) {
 			  res.send([]);
 			}
 			res.send(parsedBody.Departure.map((departure) => {
@@ -73,12 +75,93 @@ router.get('/getDepartures/:extId/:products?', function(req, res) {
 	}
 });
 
+router.get('/getLines/:extId', function(req, res) {
+	try {
+		console.info('Retrieving VBB lines at station');
+		const getLinesOptions = {
+			method: 'GET',
+			url: buildUrl('departureBoard', {
+				extId: req.params.extId,
+			}),
+			json: true,
+		};
+
+		request(getLinesOptions)
+		.then(function (parsedBody) {
+			if (!parsedBody.Departure || parsedBody.Departure === []) {
+			  res.send([]);
+			}
+			let availAbleLines = (_.sortBy(_.uniqWith(parsedBody.Departure.map((departure) => {
+				return {
+					line: departure.Product.line,
+					type: departure.Product.catOut.replace(/\s/g, ''),
+					direction: departure.direction,
+				}
+			}), _.isEqual), ['type', 'line']));
+            if (!availAbleLines || availAbleLines === []) {
+                res.send([]);
+                return
+            }
+            const getDestinationId = async (direction, index) => {
+                try {
+                    const getDestinationIdOptions = {
+                        method: 'GET',
+                        url: buildUrl('location.name', {
+                            input: direction,
+                            type: 'S',
+                        }),
+                        json: true,
+                    };
+
+                    await request(getDestinationIdOptions)
+                        .then((parsedBody) => {
+                        	console.log(parsedBody.stopLocationOrCoordLocation[0].StopLocation.id);
+                            availAbleLines[index].destinationId = parsedBody.stopLocationOrCoordLocation[0].StopLocation.extId || null;
+                        })
+                } catch (ex) {
+                    console.error('Error while getting destination ID');
+                    console.error(ex)
+                }
+            };
+            for (let i = 0; i < availAbleLines.length; i++) {
+                getDestinationId(availAbleLines[i].direction, i);
+			}
+			const time = Date.now();
+			const interval = setInterval(() => {
+                let isReady = true;
+                availAbleLines.forEach((line) => {
+                    if (!line.destinationId) {
+                        isReady = false;
+                    }
+                });
+                if (isReady) {
+                    res.send(availAbleLines);
+                    clearInterval(interval);
+                }
+                if (time - Date.now() > 10000) {
+                    res.send([]);
+                    clearInterval(interval);
+                }
+            }, 100)
+		})
+		.catch(function (e) {
+			console.error(e);
+			res.send('Error while getting VBB lines at station.');
+		});
+	} catch (ex) {
+		console.error(ex);
+		res.send('Error while getting VBB lines at station.');
+	}
+});
+
 module.exports = router;
 
 const buildUrl = (path, parameterObj = {}) => {
 	let url = vbbApiBaseUrl + path + '?accessId=' + process.env.vbbApiAccessId + '&format=json';
 	Object.keys(parameterObj).forEach((key) => {
-		url += '&' + key + '=' + parameterObj[key];
+        if (parameterObj[key]) {
+			url += '&' + key + '=' + parameterObj[key];
+        }
 	});
 	return url;
 };
