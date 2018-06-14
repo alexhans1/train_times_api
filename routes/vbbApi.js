79d2ec9eb71635f7965c0c6233aa4f276e5cc94b
alexhans1 <mail@alexanderhans.me>
2018-06-14 22:38:11 +0200

    send all departures and removed option to filter by direction

--- routes/vbbApi.js
+++ routes/vbbApi.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request-promise');
-const _ = require('lodash');
 
 const vbbApiBaseUrl = 'http://fahrinfo.vbb.de/restproxy/';
 
@@ -43,9 +42,7 @@ router.get('/getDepartures/:id/:products?', function(req, res) {
             method: 'GET',
             url: buildUrl('departureBoard', {
                 id: req.params.id,
-                // maxJourneys: 6,
                 products: req.params.products || null,
-                direction: req.query.direction || null,
             }),
             json: true,
         };
@@ -53,7 +50,9 @@ router.get('/getDepartures/:id/:products?', function(req, res) {
         request(getDeparturesOptions)
         .then(function (parsedBody) {
             if (!parsedBody.Departure || parsedBody.Departure === []) {
+                console.error('No departures', parsedBody);
                 res.send([]);
+                return
             }
             res.send(parsedBody.Departure.map((departure) => {
                 return {
@@ -68,7 +67,7 @@ router.get('/getDepartures/:id/:products?', function(req, res) {
                     direction: departure.direction,
                     trainCategory: departure.trainCategory,
                 }
-            }).slice(0,6));
+            }));
         })
         .catch(function (e) {
             console.error(e);
@@ -77,86 +76,6 @@ router.get('/getDepartures/:id/:products?', function(req, res) {
     } catch (ex) {
         console.error(ex);
         res.send({error: true, message: 'Error while retrieving VBB departures.'});
-    }
-});
-
-router.get('/getLines/:id', function(req, res) {
-    try {
-        console.info('Retrieving VBB lines at station');
-        const getLinesOptions = {
-            method: 'GET',
-            url: buildUrl('departureBoard', {
-                id: req.params.id,
-            }),
-            json: true,
-        };
-
-        request(getLinesOptions)
-        .then((parsedBody) => {
-            if (!parsedBody.Departure || parsedBody.Departure === []) {
-                res.send([]);
-                return
-            }
-            let availAbleLines = (_.sortBy(_.uniqWith(parsedBody.Departure.map((departure) => {
-                return {
-                    line: departure.Product.line,
-                    type: departure.Product.catOut.replace(/\s/g, ''),
-                    direction: departure.direction,
-                }
-            }), _.isEqual), ['type', 'line']));
-            if (!availAbleLines || availAbleLines === []) {
-                res.send([]);
-                return
-            }
-            const getDestinationId = async (direction, index) => {
-                try {
-                    const getDestinationIdOptions = {
-                        method: 'GET',
-                        url: buildUrl('location.name', {
-                            input: direction,
-                            type: 'S',
-                        }),
-                        json: true,
-                    };
-
-                    await request(getDestinationIdOptions)
-                    .then((parsedBody) => {
-                        availAbleLines[index].destinationId = parsedBody.StopLocation[0].id || null;
-                    })
-                } catch (ex) {
-                    console.error('Error while getting destination ID');
-                    console.error(ex);
-                    res.send({error: true, message: 'Error while getting VBB lines at station.'});
-                }
-            };
-            for (let i = 0; i < availAbleLines.length; i++) {
-                getDestinationId(availAbleLines[i].direction, i);
-            }
-            const time = Date.now();
-            const interval = setInterval(() => {
-                let isReady = true;
-                availAbleLines.forEach((line) => {
-                    if (!line.destinationId) {
-                        isReady = false;
-                    }
-                });
-                if (isReady) {
-                    res.send(availAbleLines);
-                    clearInterval(interval);
-                }
-                if (Date.now() - time > 2000) {
-                    res.send([]);
-                    clearInterval(interval);
-                }
-            }, 100)
-        })
-        .catch(function (e) {
-            console.error(e);
-            res.send({error: true, message: 'Error while getting VBB lines at station.'});
-        });
-    } catch (ex) {
-        console.error(ex);
-        res.send({error: true, message: 'Error while getting VBB lines at station.'});
     }
 });
 
